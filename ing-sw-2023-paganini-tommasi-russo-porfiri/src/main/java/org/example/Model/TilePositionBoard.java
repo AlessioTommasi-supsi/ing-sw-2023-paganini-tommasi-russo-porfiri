8d493193e2fb4c122e9fb855bd0bc8f22305931c
EmanueleRuss0 <emanuele1.russo@mail.polimi.it>
2023-03-19 16:50:39 +0100

    Scrittura codice classe

---
+++ ing-sw-2023-paganini-tommasi-russo-porfiri/src/main/java/org/example/Model/TilePositionBoard.java
@@ -0,0 +1,64 @@
+package org.example.Model;
+
+public class TilePositionBoard {
+    private int x;
+    private int y;
+    private TileObj tileInSlot;
+    private boolean occupied;
+
+    public TilePositionBoard(int abscissa, int ordinate){
+        this.x= abscissa;
+        this.y= ordinate;
+        this.tileInSlot= null;
+        this.occupied= false;
+    }
+
+    public TilePositionBoard(int abscissa, int ordinate, TileObj tile){
+        this.x= abscissa;
+        this.y= ordinate;
+        this.tileInSlot= tile;
+        this.occupied= true;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public boolean isOccupied(){
+        if(this.occupied==true){
+            return true;
+        }
+        else return false;
+    }
+
+    public TileObj getTile(){
+        if(tileInSlot != null){
+            return this.tileInSlot;
+        }
+        else throw new NullPointerException();
+    }
+
+    public void setTile(TileObj tile) throws Exception {
+        if(this.occupied != true){
+            this.tileInSlot= tile;
+            this.occupied= true;
+        }
+        else throw new Exception("tile already occupied, choose another TilePositionBoard");
+    }
+
+    public TileObj removeTile(){
+        if(this.tileInSlot != null){
+            TileObj tempTile = new TileObj(this.tileInSlot);
+            this.tileInSlot= null;
+            this.occupied=false;
+            return tempTile;
+        }
+        else throw new NullPointerException();
+    }
+
+
+}
