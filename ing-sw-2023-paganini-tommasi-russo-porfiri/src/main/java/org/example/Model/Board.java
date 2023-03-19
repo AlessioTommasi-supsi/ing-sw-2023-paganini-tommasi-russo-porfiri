fcf901507caad14bae2f85934d0f0ef48efafd80
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2023-03-19 08:22:43 +0100

    Update Board.java

--- ing-sw-2023-paganini-tommasi-russo-porfiri/src/main/java/org/example/Model/Board.java
+++ ing-sw-2023-paganini-tommasi-russo-porfiri/src/main/java/org/example/Model/Board.java
@@ -6,41 +6,55 @@ import java.util.*;
  * 
  */
 public class Board {
+    private TilePosition placement[];
+    private TileObjBag bag;
+    private CommonDeck commonDeck;
 
-    /**
-     * Default constructor
-     */
-    public Board() {
+
+    //sara il gioco a creare i placement giusti in base a quante persone ho!!
+    public Board(TilePosition[] placement, TileObjBag bag, CommonDeck commonDeck) {
+        this.placement = placement;
+        this.bag = bag;
+        this.commonDeck = commonDeck;
     }
 
-    /**
-     * 
-     */
-    public void Board() {
-        // TODO implement here
+    public TilePosition[] getPlacement() {
+        return placement;
     }
 
-    /**
-     * @return
-     */
+    public void setPlacement(TilePosition[] placement) {
+        this.placement = placement;
+    }
+
+    public TileObjBag getBag() {
+        return bag;
+    }
+
+    public void setBag(TileObjBag bag) {
+        this.bag = bag;
+    }
+
+    public CommonDeck getCommonDeck() {
+        return commonDeck;
+    }
+
+    public void setCommonDeck(CommonDeck commonDeck) {
+        this.commonDeck = commonDeck;
+    }
+
+
+
+
     public Set<TilePosition> displayBoard() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @param p 
-     * @return
-     */
+
     public void deleteTiles(Set<TilePosition> p) {
         // TODO implement here
     }
 
-    /**
-     * @param t 
-     * @param x 
-     * @param y
-     */
     public void setTile(TileObj t, int x, int y) {
         // TODO implement here
     }
