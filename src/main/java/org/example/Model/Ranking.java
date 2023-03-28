397e3e8abaa8bd6e8e916c936f8e8548d7d0ea01
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2023-03-28 04:08:59 +0200

    new class

---
+++ src/main/java/org/example/Model/Ranking.java
@@ -0,0 +1,16 @@
+package org.example.Model;
+
+import java.util.*;
+
+
+public class Ranking {
+    private int punteggio;
+    private Player player;
+
+    public Ranking(Player p, int points) {
+        punteggio = points;
+        player =p;
+    }
+    
+
+}
