394cece05db17eec866242f411307b6910878e12
Mattia-Paganini <mattia.paganini99@gmail.com>
2023-04-04 22:55:29 +0200

    implementazione diagonal

--- src/main/java/org/example/Model/CommonCardColumnAll.java
+++ src/main/java/org/example/Model/CommonCardColumnAll.java
@@ -1,6 +1,128 @@
 package org.example.Model;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class CommonCardColumnAll implements CommonObjectiveInterface {
+    List<TileType> tileTypes = new List<TileType>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<TileType> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(TileType tileType) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends TileType> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection<? extends TileType> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public TileType get(int index) {
+            return null;
+        }
+
+        @Override
+        public TileType set(int index, TileType element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, TileType element) {
+
+        }
+
+        @Override
+        public TileType remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<TileType> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<TileType> listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List<TileType> subList(int fromIndex, int toIndex) {
+            return null;
+        }
+    };
+
     public boolean executeAlgorithm(String nameOfCard) {
         int counterLine = 0;
         if (nameOfCard == "Vertical different six") {
--- src/main/java/org/example/Model/CommonCardPosition.java
+++ src/main/java/org/example/Model/CommonCardPosition.java
@@ -1,57 +1,110 @@
 package org.example.Model;
 
-import java.sql.Array;
+
+import java.util.List;
 
 public class CommonCardPosition implements CommonObjectiveInterface {
 
     int countTiles = 0;
     private Shelves shelf;
+    boolean objectiveAchieved = false;
 
     public boolean executeAlgorithm(String nameOfCard) {
         int counterTile = 0;
-        if (nameOfCard == "Angles") {
-            if ((shelf.getTilePosition(0,0) != null)
-                || (shelf.getTilePosition(0, 5) != null)
-                    || (shelf.getTilePosition(4, 0) != null)
-                    || (shelf.getTilePosition(4, 5) != null)) { counterTile++;}
-            if (counterTile == 4) {
-                return true;
-            } else { return false;}
-
-        } else if (nameOfCard == "Eight tiles no restriction") {
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 5 ; j++) {
-                    if (shelf.getTilePosition(i, j) != null) {
-                        counterTile++;
+        switch (nameOfCard) {
+            case "Angles" -> {
+                if ((shelf.getTilePosition(0, 0) != null)
+                        && (shelf.getTilePosition(0, 5) != null)
+                        && (shelf.getTilePosition(4, 0) != null)
+                        && (shelf.getTilePosition(4, 5) != null)) {
+                    objectiveAchieved = true;
+                }
+                return objectiveAchieved;
+            }
+            case "Eight tiles no restriction" -> {
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 6; j++) {
+                        if (shelf.getTilePosition(i, j) != null) {
+                            counterTile++;
+                        }
                     }
                 }
-            }
-            if (counterTile == 8) {
-                return true;
-            } else { return false; }
-        } else if (nameOfCard == "Inferior diagonal") {
-            int i, j = 0;
-            int indexDecrescent = 5;
-            for (i = 0; i < 6; i++) {
-                for (j = 0; j < indexDecrescent; j++) {
-                    if (shelf.getTilePosition(i, j) == null) {
-                        break;
-                    } else { counterTile++; }
+                if (counterTile == 8) {
+                    return true;
+                } else {
+                    return false;
                 }
-                indexDecrescent--;
             }
-            if (counterTile == 15) { return true; }
-            else { return false; }
-        } else if (nameOfCard == "Diagonal") {
-            if ((shelf.getTilePosition(4, 0) != null)
-            || (shelf.getTilePosition(3, 1 ) != null)
-            || (shelf.getTilePosition(2, 2) != null)
-            || (shelf.getTilePosition(1, 3) != null)
-            || (shelf.getTilePosition(0, 4) != null)) {
-                counterTile++;
+            case "Inferior diagonal" -> {
+                int i, j = 0;
+                int indexDecrescent = 6;
+                for (i = 0; i < 5; i++) {
+                    for (j = 0; j < indexDecrescent; j++) {
+                        if (shelf.getTilePosition(i, j) == null) {
+                            break;
+                        } else {
+                            counterTile++;
+                        }
+                    }
+                    indexDecrescent--;
+                }
+                if (counterTile == 15) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-            if (counterTile == 5) { return true; }
-            else { return false; }
+            case "Diagonal" -> {
+                if (shelf.getTilePosition(0, 0) != null) {
+                    TileType typeTile = shelf.getTilePosition(0, 0).getTile().getType();
+                    if ((shelf.getTilePosition(1, 1) != null) && (shelf.getTilePosition(1, 1).getTile().getType() == typeTile)) {
+                        if ((shelf.getTilePosition(2, 2) != null) && (shelf.getTilePosition(2, 2).getTile().getType() == typeTile)) {
+                            if ((shelf.getTilePosition(3, 3) != null) && (shelf.getTilePosition(3, 3).getTile().getType() == typeTile)) {
+                                if ((shelf.getTilePosition(4, 4) != null) && (shelf.getTilePosition(4, 4).getTile().getType() == typeTile)) {
+                                    objectiveAchieved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (shelf.getTilePosition(0, 1) != null) {
+                    TileType typeTile = shelf.getTilePosition(1, 2).getTile().getType();
+                    if ((shelf.getTilePosition(1, 2) != null) && (shelf.getTilePosition(1, 2).getTile().getType() == typeTile)) {
+                        if ((shelf.getTilePosition(2, 3) != null) && (shelf.getTilePosition(2, 3).getTile().getType() == typeTile)) {
+                            if ((shelf.getTilePosition(3, 4) != null) && (shelf.getTilePosition(3, 4).getTile().getType() == typeTile)) {
+                                if ((shelf.getTilePosition(4, 5) != null) && (shelf.getTilePosition(4, 5).getTile().getType() == typeTile)) {
+                                    objectiveAchieved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (shelf.getTilePosition(4, 0) != null) {
+                    TileType typeTile = shelf.getTilePosition(4, 0).getTile().getType();
+                    if ((shelf.getTilePosition(3, 1) != null) && (shelf.getTilePosition(3, 1).getTile().getType() == typeTile)) {
+                        if ((shelf.getTilePosition(2, 2) != null) && (shelf.getTilePosition(2, 2).getTile().getType() == typeTile)) {
+                            if ((shelf.getTilePosition(1, 3) != null) && (shelf.getTilePosition(1, 3).getTile().getType() == typeTile)) {
+                                if ((shelf.getTilePosition(0, 4) != null) && (shelf.getTilePosition(0, 4).getTile().getType() == typeTile)) {
+                                    objectiveAchieved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (shelf.getTilePosition(4, 1) != null) {
+                    TileType typeTile = shelf.getTilePosition(4, 1).getTile().getType();
+                    if ((shelf.getTilePosition(3, 2) != null) && (shelf.getTilePosition(3, 2).getTile().getType() == typeTile)) {
+                        if ((shelf.getTilePosition(2, 3) != null) && (shelf.getTilePosition(2, 3).getTile().getType() == typeTile)) {
+                            if ((shelf.getTilePosition(1, 4) != null) && (shelf.getTilePosition(1, 4).getTile().getType() == typeTile)) {
+                                if ((shelf.getTilePosition(0, 5) != null) && (shelf.getTilePosition(0, 5).getTile().getType() == typeTile)) {
+                                    objectiveAchieved = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                return objectiveAchieved;
+            }
         }
         return false;
     }
