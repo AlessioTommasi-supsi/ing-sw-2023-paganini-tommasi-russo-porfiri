cbddc89c431bb4ba37c3508d8d003951531f7ca6
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2023-05-08 18:04:03 +0200

    fix merge

--- src/main/java/org/example/Model/Game.java
+++ src/main/java/org/example/Model/Game.java
@@ -14,15 +14,15 @@ public abstract class Game implements Serializable {
     private Player current_player;
     private int current_game_id;
 
-    protected Board board;
-    protected ArrayList<Player> players;//ordinato in base all ordine nel quale i giocaori si sono uniti alla patrita e quindi anche in ordine di chi tocca a giocare!
-    protected int playerNumber;//numeri di giocatori che ci sono in questa partita!
-    protected Player dealer;
-    protected StatoPartita stato;
+    private Board board;
+    private ArrayList<Player> players;//ordinato in base all ordine nel quale i giocaori si sono uniti alla patrita e quindi anche in ordine di chi tocca a giocare!
+    private int playerNumber;//numeri di giocatori che ci sono in questa partita!
+    private Player dealer;
+    private StatoPartita stato;
 
-    protected int ranking[]=null;
+    private int ranking[]=null;
 
-    protected ArrayList<Ranking> rank;
+    private ArrayList<Ranking> rank;
 
     public Game(int playerNumber, Player mazziere) {
         this.playerNumber = playerNumber;
@@ -144,7 +144,7 @@ public abstract class Game implements Serializable {
     }
 
     public Board getBoard() {
-        return board;
+        return new Board(board);
     }
 
     public Player getPlayer(int id_player) {
--- src/main/java/org/example/Model/GameFourPlayers.java
+++ src/main/java/org/example/Model/GameFourPlayers.java
@@ -29,7 +29,6 @@ public class GameFourPlayers extends Game implements Serializable {
         tempP.add(new TilePositionBoard(7,5));
         tempP.add(new TilePositionBoard(8,4));
 
-        this.getInstanceBoard().setPlacements(tempP);
-        this.getInstanceBoard().addTiles();
+        setBoard(tempP);
     }
 }
