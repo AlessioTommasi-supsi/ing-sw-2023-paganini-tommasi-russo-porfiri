3225855c716238495f93640e210e4324681f6f00
Mattia-Paganini <mattia.paganini99@gmail.com>
2023-06-01 14:11:25 +0200

    evolutivo aggiunti test su diagonal e CommonCard8Tiles e sistemate le classi corrispondenti

--- src/main/java/org/example/model/CommonCard8Tiles.java
+++ src/main/java/org/example/model/CommonCard8Tiles.java
@@ -21,37 +21,60 @@ public class CommonCard8Tiles extends CommonCard implements Serializable {
         for(int i = 0; i < 6; i++) {
             for (int j=0;j<5;j++) {
                 if (player.getShelves().getTilePosition(i, j).isOccupied()) {
-                    switch (player.getShelves().getTilePosition(i, j).getTile().getType().toString()) {
-                        case "C":
+                    System.out.println("Book size");
+                    System.out.println(listOfBooks.size());
+                    System.out.println("Cat size");
+                    System.out.println(listOfCats.size());
+                    System.out.println("Frame size");
+                    System.out.println(listOfFrames.size());
+                    System.out.println("Plant size");
+                    System.out.println(listOfPlants.size());
+                    System.out.println("Trophy size");
+                    System.out.println(listOfTrophies.size());
+                    System.out.println("Games size");
+                    System.out.println(listOfGames.size());
+                    System.out.println("Position: " + player.getShelves().getTilePosition(i, j).getX() + " " + player.getShelves().getTilePosition(i, j).getY());
+                    System.out.println("Type: " + player.getShelves().getTilePosition(i, j).getTile().getType());
+                    System.out.println("------");
+                    TileType type = player.getShelves().getTilePosition(i, j).getTile().getType();
+                    switch (type) {
+
+                        case CAT -> {
                             listOfCats.add(player.getShelves().getTilePosition(i, j));
                             if (listOfCats.size() == 8) {
                                 return true;
                             }
-                        case "B":
+                        }
+                        case BOOK -> {
                             listOfBooks.add(player.getShelves().getTilePosition(i, j));
                             if (listOfBooks.size() == 8) {
                                 return true;
                             }
-                        case "F":
+                        }
+                        case FRAME -> {
                             listOfFrames.add(player.getShelves().getTilePosition(i, j));
                             if (listOfFrames.size() == 8) {
                                 return true;
                             }
-                        case "P":
+                        }
+                        case PLANT -> {
                             listOfPlants.add(player.getShelves().getTilePosition(i, j));
                             if (listOfPlants.size() == 8) {
                                 return true;
                             }
-                        case "T":
+                        }
+                        case TROPHY -> {
                             listOfTrophies.add(player.getShelves().getTilePosition(i, j));
                             if (listOfTrophies.size() == 8) {
                                 return true;
                             }
-                        case "G":
+                        }
+                        case GAMES -> {
                             listOfGames.add(player.getShelves().getTilePosition(i, j));
                             if (listOfGames.size() == 8) {
                                 return true;
                             }
+                        }
                     }
                 }
             }
--- src/test/java/org/test/CommonCardDiagonalTest.java
+++ src/test/java/org/test/CommonCardDiagonalTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CommonCardDiagonalTest { /*da completare con altre diagonali*/
 
     @Test
-    public void testExecuteAlgorithm() {
+    public void testExecuteAlgorithmDiagonalLeftFirst() {
         Player player = new Player("Player1");
 
         player.getShelves().addTile(new TilePositionShelves(0,0, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
@@ -25,6 +25,122 @@ public class CommonCardDiagonalTest { /*da completare con altre diagonali*/
         player.getShelves().addTile(new TilePositionShelves(2,4, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
         player.getShelves().addTile(new TilePositionShelves(3,4, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
         player.getShelves().addTile(new TilePositionShelves(4,4, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+
+        CommonCard commonCard = new CommonCardDiagonal(1);
+
+        boolean result = commonCard.executeAlgorithm(player);
+        assertTrue(result, "you have to complete one of the possible diagonals with the same type of tiles");
+    }
+
+    @Test
+    public void testExecuteAlgorithmDiagonalLeftFalseFirst() {
+        Player player = new Player("Player1");
+
+        player.getShelves().addTile(new TilePositionShelves(0,0, new TileObj(TileType.FRAME, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,1, new TileObj(TileType.BOOK, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(1,1, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(0,2, new TileObj(TileType.FRAME, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(1,2, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2,2, new TileObj(TileType.CAT, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(0,3, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(1,3, new TileObj(TileType.GAMES, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(2,3, new TileObj(TileType.BOOK, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(3,3, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,4, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(1,4, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2,4, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(3,4, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(4,4, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+
+        CommonCard commonCard = new CommonCardDiagonal(1);
+
+        boolean result = commonCard.executeAlgorithm(player);
+        assertFalse(result, "you have to complete one of the possible diagonals with the same type of tiles");
+    }
+
+    @Test
+    public void testExecuteAlgorithmDiagonalRightFirst() {
+        Player player = new Player("Player1");
+
+        player.getShelves().addTile(new TilePositionShelves(0, 4, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0, 3, new TileObj(TileType.BOOK, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(1, 3, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(0, 2, new TileObj(TileType.FRAME, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(1, 2, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2, 2, new TileObj(TileType.CAT, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(0, 1, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(1, 1, new TileObj(TileType.GAMES, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(2, 1, new TileObj(TileType.BOOK, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(3, 1, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0, 0, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(1, 0, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2, 0, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(3, 0, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(4, 0, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+
+        CommonCard commonCard = new CommonCardDiagonal(1);
+
+        boolean result = commonCard.executeAlgorithm(player);
+        assertTrue(result, "you have to complete one of the possible diagonals with the same type of tiles");
+    }
+
+    @Test
+    public void testExecuteAlgorithmLeftSecond() {
+        Player player = new Player("Player1");
+        //first line
+        player.getShelves().addTile(new TilePositionShelves(0,0, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,1, new TileObj(TileType.FRAME, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,2, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,3, new TileObj(TileType.GAMES, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,4, new TileObj(TileType.PLANT, TileVariant.VARIANT_THREE)));
+        //diagonal
+        player.getShelves().addTile(new TilePositionShelves(1,0, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(1,1, new TileObj(TileType.BOOK, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(2,1, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(1,2, new TileObj(TileType.FRAME, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(2,2, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(3,2, new TileObj(TileType.CAT, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(1,3, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2,3, new TileObj(TileType.GAMES, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(3,3, new TileObj(TileType.BOOK, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(4,3, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(1,4, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2,4, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(3,4, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(4,4, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(5,4, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+
+        CommonCard commonCard = new CommonCardDiagonal(1);
+
+        boolean result = commonCard.executeAlgorithm(player);
+        assertTrue(result, "you have to complete one of the possible diagonals with the same type of tiles");
+    }
+
+    @Test
+    public void testExecuteAlgorithmDiagonalRightSecond() {
+        Player player = new Player("Player1");
+
+        player.getShelves().addTile(new TilePositionShelves(0,0, new TileObj(TileType.FRAME, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,1, new TileObj(TileType.GAMES, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,2, new TileObj(TileType.PLANT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,3, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(0,4, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
+
+        player.getShelves().addTile(new TilePositionShelves(1, 4, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(1, 3, new TileObj(TileType.BOOK, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(2, 3, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(1, 2, new TileObj(TileType.FRAME, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(2, 2, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(3, 2, new TileObj(TileType.CAT, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(1, 1, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2, 1, new TileObj(TileType.GAMES, TileVariant.VARIANT_TWO)));
+        player.getShelves().addTile(new TilePositionShelves(3, 1, new TileObj(TileType.BOOK, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(4, 1, new TileObj(TileType.CAT, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(1, 0, new TileObj(TileType.FRAME, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(2, 0, new TileObj(TileType.GAMES, TileVariant.VARIANT_ONE)));
+        player.getShelves().addTile(new TilePositionShelves(3, 0, new TileObj(TileType.BOOK, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(4, 0, new TileObj(TileType.TROPHY, TileVariant.VARIANT_THREE)));
+        player.getShelves().addTile(new TilePositionShelves(5, 0, new TileObj(TileType.CAT, TileVariant.VARIANT_ONE)));
 
         CommonCard commonCard = new CommonCardDiagonal(1);
 
