7614f291f47d30c672cab867567d9ab21855dc99
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2023-06-30 02:34:04 +0200

    FUNZIONANTE

--- src/main/java/org/project/ingsw2023PaganiniTommasiRussoPorfiri/view/TextualUI/ViewMyShelfie.java
+++ src/main/java/org/project/ingsw2023PaganiniTommasiRussoPorfiri/view/TextualUI/ViewMyShelfie.java
@@ -357,10 +357,6 @@ public class ViewMyShelfie extends Observable<ChoiceMyShelfie> implements Runnab
                                 break;
                             }
                         }
-
-                        //tilesToRemove.get(0).setTile(new TileObj(TileType.BOOK, TileVariant.VARIANT_ONE));
-
-
                     }
 
                     int order[] = new int[tilesToRemove.size()];
@@ -711,13 +707,11 @@ public class ViewMyShelfie extends Observable<ChoiceMyShelfie> implements Runnab
                 try {
                     this.initComponentGui();
                     if (this.currentGame != null) {
-                        //this.initComponentGui();
                         this.controller.setEnableSendButton(this.currentGame.getCurrentPlayer().getUsername().equals(this.player.getUsername()));
                         this.controller.UpdateCommonPoints(this.currentGame.getCommon1(), this.getCurrentGame().getCommon2());
                         this.controller.updateBoard(this.currentGame.getBoard().getPlacements());
                         this.controller.updateShelves(this.currentGame.getPlayers());
                         //se e il mio turno attivo il bottone send inoltre verifico che non ho gia pescato
-
                     }
                 }catch (Exception e) {
                     //.DEBUG
